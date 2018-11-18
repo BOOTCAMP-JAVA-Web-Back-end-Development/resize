bbcc24d0af9981b5d119d97a498d5c10acb4b236
thanh <thanhdc.consulting@gmail.com>
2018-11-19 05:09:35 +0700

    done

---
+++ src/tester.java
@@ -0,0 +1,32 @@
+import shape.Circle;
+import shape.Rectangle;
+import shape.Shape;
+import shape.Square;
+
+public class tester {
+    public static void main(String[] args) {
+        Shape[] shape = new Shape[10];
+        for (int i = 0; i < 10; i++) {
+            shape[i] = getRandomShape();
+        }
+        Shape.printShape(shape);
+        for (Shape x: shape){
+            x.resize(Math.random()*100);
+        }
+        System.out.println("\n");
+        Shape.printShape(shape);
+    }
+
+    static Shape getRandomShape() {
+        int radom = (int) (Math.round(Math.random() * 2));
+        switch (radom) {
+            case 0:
+                return new Square(10);
+            case 1:
+                return new Rectangle(10, 20);
+            default:
+                return new Circle(10);
+        }
+    }
+
+}
